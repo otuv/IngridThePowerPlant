38cab9176c3cbbd3262233cc6def3c66a4cb1556
Otuv <otuvesson@gmail.com>
2016-04-11 22:30:15 +0200

    Version 0.4
    Moistlevels normalized

--- Scripts/Actions/readmoist.lua
+++ Scripts/Actions/readmoist.lua
@@ -8,7 +8,16 @@ for i=1,5 do -- blink green light
     tmr.delay(1 * 500 * 1000)
 end
 gpio.write(2,gpio.LOW)
-moistVal = adc.read(0)
-print('moistVal = ' .. tostring(moistVal))
+readVal = adc.read(0)
+print('moistVal = ' .. tostring(readVal))
 gpio.write(1,gpio.LOW)
-return moistVal
+
+if readVal >= 150 then
+    outVal = 150
+elseif readVal <= 50 then
+    outVal = 50
+else
+    outVal = (readVal - 150)*-1
+end
+print('converted to: ' .. outVal)
+return outVal
