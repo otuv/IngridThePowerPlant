800d8129d034e8f768b8b4773996b36a1d7331de
Otuv <otuvesson@gmail.com>
2016-04-11 22:49:42 +0200

    Version 0.4.1
    Fix can only tweet value

--- Scripts/Actions/tweet.lua
+++ Scripts/Actions/tweet.lua
@@ -1,7 +1,7 @@
 local tweetVal, tweetEvent = ...
 print('tweetVal = ' .. tweetVal)
 print('tweetEvent = ' .. tweetEvent)
-valueString = "\"value1\":" .. tweetVal
+valueString = "\"value1\":\"" .. tweetVal .. "\""
 valueString = "{" .. valueString .. "}"
 print(valueString)
 intStringLength = string.len(valueString)
@@ -12,7 +12,6 @@ if wifi.sta.status() == 5 then
     gpio.write(2,gpio.LOW)
     print('Wifi ok ' .. wifi.sta.getip())
 
-    moist = dofile('readmoist.lua') -- get moist reading
     makerChannelKey = dofile("makerchannelkey.lua") -- get makerchannel key
     gpio.write(2,gpio.HIGH)
     tmr.delay(1 * 500 * 1000)
@@ -32,12 +31,6 @@ if wifi.sta.status() == 5 then
                     "Content-Type: application/json\r\n" ..
                     "Content-Length: " .. intStringLength .. "\r\n\r\n" .. valueString .. "\r\n"
         ) end)
-        --conn:send("POST /trigger/" .. tweetEvent .. "/with/key/oTS9rXKLzheS0YN78Hlcg5MQYvEfFwDcFS8hXvjB2t6 HTTP/1.1\r\n")
-        --conn:send("Host: maker.ifttt.com\r\n") 
-        --conn:send("Accept: */*\r\n") 
-        --conn:send("Content-Type: application/json\r\n")
-        --conn:send("Content-Length: " .. intStringLength .. "\r\n\r\n" .. valueString .. "\r\n")
-
     conn:on("disconnection", function(conn, payload) print('Disconnected') end)
     
     conn:connect(80,"maker.ifttt.com")
