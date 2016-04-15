6620fc7593da853dffa0fd8932e438dbc5c98f39
Otuv <otuvesson@gmail.com>
2016-04-15 23:32:42 +0200

    0.5.1 Mulitple moisture readings

--- Scripts/Actions/readmoist.lua
+++ Scripts/Actions/readmoist.lua
@@ -1,23 +1,56 @@
--- Read values
+-- prepare pin
 gpio.mode(1,gpio.OUTPUT)
-gpio.write(1,gpio.HIGH)
-for i=1,5 do -- blink green light
+
+-- prepare collector
+local readVals = {}
+local noi = 5 -- number of iterations
+
+for i=1,noi do -- blink green light while reading
     gpio.write(2,gpio.HIGH)
-    tmr.delay(1 * 500 * 1000)
+    gpio.write(1,gpio.HIGH) -- power on
+    tmr.delay(1 * 500 * 1000) -- wait
+    readVals[i] = adc.read(0) -- write reading to array
+    gpio.write(1,gpio.LOW) -- power down
     gpio.write(2,gpio.LOW)
     tmr.delay(1 * 500 * 1000)
 end
 gpio.write(2,gpio.LOW)
-readVal = adc.read(0)
-print('moistVal = ' .. tostring(readVal))
-gpio.write(1,gpio.LOW)
 
-if readVal >= 150 then
+print('table.getn(readVals) ' .. table.getn(readVals))
+
+for k,v in ipairs(readVals) do
+    print('value ' .. k .. ' = ' .. v)
+end
+
+-- filter values
+table.sort(readVals) 
+table.remove(readVals, noi)
+table.remove(readVals, 1)
+
+for k,v in ipairs(readVals) do
+    print('value ' .. k .. ' = ' .. v)
+end
+
+-- calculate average
+local totalMoisture = 0
+for k,v in ipairs(readVals) do
+    totalMoisture = totalMoisture + v
+    print('total moisture at ' .. k .. ' = ' .. totalMoisture)
+end
+local averageMoisture = totalMoisture/table.getn(readVals)
+
+print('averageMoisture = ' .. tostring(averageMoisture))
+
+local outVal = 0
+if averageMoisture >= 150 then
     outVal = 0
-elseif readVal <= 50 then
+    print('outVal >= 150 ' .. outVal)
+elseif averageMoisture <= 50 then
     outVal = 100
+    print('outVal <= 50 ' .. outVal)
 else
-    outVal = (readVal - 150)*-1
+    outVal = (averageMoisture - 150)*-1
+    print('outVal (averageMoisture - 150)*-1 = ' .. outVal)
 end
 print('converted to: ' .. outVal)
 return outVal
